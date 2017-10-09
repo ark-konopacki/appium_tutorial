233bff178d64b1bf35b70473026eec600c42e647
Arkadiusz Konopacki <ark.konopacki@gmail.com>
2017-10-09 13:09:20 +0200

    Remove unused import and disabling info loggs

--- src/test/java/appium/AndroidTest.java
+++ src/test/java/appium/AndroidTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.util.List;
 
 
 public class AndroidTest extends BaseDriver{
--- src/test/java/appium/BaseDriver.java
+++ src/test/java/appium/BaseDriver.java
@@ -4,8 +4,6 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.service.local.AppiumDriverLocalService;
-import io.appium.java_client.service.local.AppiumServerHasNotBeenStartedLocallyException;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebElement;
@@ -14,12 +12,27 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class BaseDriver extends JUnitReporter{
-    public AppiumDriver<WebElement> driver;
-    
+	
+	private static final Logger[] pin;
+	static {
+	    pin = new Logger[]{
+	        Logger.getLogger("org.apache.commons.httpclient"),
+	        Logger.getLogger("org.openqa.selenium.remote.ProtocolHandshake")
+	    };
 
+	    for (Logger l : pin) {
+	        l.setLevel(Level.OFF);
+	    }
+	}
+    
+	
+	public AppiumDriver<WebElement> driver;
+    
     @Before
     public void setUp() throws Exception {
         File classpathRoot = new File(System.getProperty("user.dir"));
@@ -27,7 +40,7 @@ public class BaseDriver extends JUnitReporter{
         File app = new File(appDir.getCanonicalPath(), "Financius.v0.18.2.apk");
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("deviceName", ""); // device UID from adb
-		capabilities.setCapability("platformVersion", ""); // android version
+		capabilities.setCapability("platformVersion", "6.0.1"); // android version
 		capabilities.setCapability("platformName", "Android");
 		capabilities.setCapability("app", app.getAbsolutePath());
 		capabilities.setCapability("appPackage", "com.code44.finance"); // package name
